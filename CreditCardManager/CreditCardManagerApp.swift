f996c36cb83b006cf2c88b2be78329a4c9773d4a
Aniket <heyaniket@hotmail.com>
2025-09-06 00:28:21 +0530

    add: Xcode project source code structure

---
+++ CreditCardManager/CreditCardManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardManagerApp.swift
+//  CreditCardManager
+//
+//  Created by Aniket's Mac on 06/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CreditCardManagerTests/CreditCardManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardManagerTests.swift
+//  CreditCardManagerTests
+//
+//  Created by Aniket's Mac on 06/09/25.
+//
+
+import Testing
+@testable import CreditCardManager
+
+struct CreditCardManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
